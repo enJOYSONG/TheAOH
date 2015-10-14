697a1e81ba41eade03bcc9038a80a15770488897
enJOYSONG <tmdgml5156@naver.com>
2015-10-15 02:37:51 +0900

    enum edit

--- TheAdventureOfHero/Base/BaseGameWorldPlace.h
+++ TheAdventureOfHero/Base/BaseGameWorldPlace.h
@@ -1,13 +1,16 @@
+#ifndef BASEGAMEWORLDPLACE_H
+#define BASEGAMEWORLDPLACE_H
 //GamePlace interface
 //set place type by enum
 enum PlaceType {
-	NONE_PLACE,
-	PLACE_INN,
-	PLACE_DUNGEON,
-	PLACE_PUB
+	NONE_PLACE = 0,
+	PLACE_INN = 1,
+	PLACE_DUNGEON = 2,
+	PLACE_PUB = 3,
 };
 
 class IBaseGameWorldPlace {
 protected:
 	PlaceType place_type_;
 };
+#endif
